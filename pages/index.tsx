66d8046de7ba519eedd183e3ce5f9450b8c45a4c
Tulsi Prasad <tulsi.prasad50@gmail.com>
2022-09-26 20:24:02 +0530

    Add basic css normalization

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,35 +45,37 @@ export default function Home() {
         <meta content={description} property="og:description" />
         <meta content="https://angle.so" property="og:url" />
       </Head>
-      <div>
-        <div className="absolute w-full">
-          <nav className="max-w-6xl mx-auto px-6 mt-12 mb-4 flex-col items-center justify-center text-center">
-            <a href="/">
-              <div className="flex flex-row justify-center">
-                <img src="/static/logo.svg" alt="React Email" className="h-10" />
-              </div>
-            </a>
-          </nav>
-        </div>
-      </div>
-      <div className="flex min-h-screen flex-col justify-center">
-        <div className="mx-auto max-w-5xl items-center justify-center">
-          <div className="flex flex-col text-center px-4">
-            <h1 className="text-gray-12 text-7xl font-extrabold mb-1">
-              Join the Waitlist.
-            </h1>
-            <p className="text-gray-11 text-2xl mb-8 font-medium">
-            Be one of the first ones to try the next-gen email experience.
-            </p>
-            <form className="flex flex-row justify-center items-center" onSubmit={onFormSubmit}>
-              <input type="email" name="email" placeholder="Email" className={["w-2/5 outline-none bg-gray-3 hover:bg-gray-4 focus:bg-gray-4 text-sm px-4 h-10 rounded-2xl", styles.email].join(" ")} required />
-              <button className="ml-3 rounded-2xl outline-none flex items-center justify-center bg-gray-3 hover:bg-gray-4 p-2" disabled={isSaving}>
-                  <img src="/static/submit.svg" className='block scale-75' alt="Submit Icon" />
-              </button>
-            </form>
+      <main>
+        <div>
+          <div className="absolute w-full">
+            <nav className="max-w-6xl mx-auto px-6 mt-12 mb-4 flex-col items-center justify-center text-center">
+              <a href="/">
+                <div className="flex flex-row justify-center">
+                  <img src="/static/logo.svg" alt="React Email" className="h-10" />
+                </div>
+              </a>
+            </nav>
           </div>
         </div>
-      </div>
+        <div className="flex min-h-screen flex-col justify-center">
+          <div className="mx-auto max-w-5xl items-center justify-center">
+            <div className="flex flex-col text-center px-4">
+              <h1 className="text-gray-12 text-7xl font-extrabold mb-1">
+                Join the Waitlist.
+              </h1>
+              <p className="text-gray-11 text-2xl mb-8 font-medium">
+              Be one of the first ones to try the next-gen email experience.
+              </p>
+              <form className="flex flex-row justify-center items-center" onSubmit={onFormSubmit}>
+                <input type="email" name="email" placeholder="Email" className={["w-2/5 outline-none bg-gray-3 hover:bg-gray-4 focus:bg-gray-4 text-sm px-4 h-10 rounded-2xl", styles.email].join(" ")} required />
+                <button className="ml-3 rounded-2xl outline-none flex items-center justify-center bg-gray-3 hover:bg-gray-4 p-2" disabled={isSaving}>
+                    <img src="/static/submit.svg" className='block scale-75' alt="Submit Icon" />
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
