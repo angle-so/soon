ecf6ebf119e68af286439a127079aa7234cd4d4c
Sagnik Sahoo <sahoosagnik1@gmail.com>
2022-09-27 18:36:27 +0530

    adding airtable api

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,35 +1,47 @@
 import React from 'react';
 import Head from 'next/head';
 import styles from "../styles/Index.module.css";
+import {useRef,useState} from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import useSound from "use-sound";
+import axios from 'axios';
+
 
 export default function Home() {
- 
-  const [isSaving, setIsSaving] = React.useState(false);
-  const [submitLabel, setSubmitLabel] = React.useState('Notify Me');
-  
-  const onFormSubmit = async (e) => {
-    try {
-      e.preventDefault();
-      setIsSaving(true);
-      setSubmitLabel('Done!');
+  const emailRef = useRef();
+  const recaptchaRef = useRef();
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState({});
 
-      const isProd = process.env.NODE_ENV === 'production';
-      const base = isProd ? 'https://angle.so' : 'http://localhost:3000';
+  const [play, { stop }] = useSound("/success.mp3", { volume: 0.4 });
 
-      await fetch(`${base}/api/save`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          email_address: e.target.email.value
-        }),
-      });
-    }
-    catch(e) {
-      alert('Something went wrong, try again later.');
-    }
-    finally {
-      e.target.reset();
-      setIsSaving(false);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    stop();
+    if (!event.target.email.value) {
+      emailRef.current.focus();
+    } else {
+      setLoading(true)
+      setResult({})
+
+      const token = await recaptchaRef.current.executeAsync();
+
+      axios.post('/api/join', { token, email: event.target.email.value })
+        .then(response => {
+          setResult(response.data);
+          setLoading(false);
+
+          if (response.data.success) play();
+        })
+        .catch(error => {
+          setResult({
+            success: false,
+            message: "System error! Please try again later."
+          });
+          setLoading(false);
+        });
+
+      recaptchaRef.current.reset();
     }
   }
 
@@ -70,10 +82,24 @@ export default function Home() {
               </p>
               <form className="flex flex-row justify-center items-center relative">
                 <svg className="text-gray-600 relative w-6 h-6 left-10" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5l-8-5h16zm0 12H4V8l8 5l8-5v10z"/></svg>
-                <input type="email" name="email" placeholder="Email" className=" pl-11 w-4/6 text-lg outline-none bg-neutral-800  hover:bg-gray-4 focus:bg-gray-4 px-4 h-10 rounded-2xl placeholder:text-gray-500 text-white left-16" required autoComplete="off"/>
+                <input type="email" name="email" placeholder="Email" className=" pl-11 w-4/6 text-lg outline-none bg-neutral-800  hover:bg-gray-4 focus:bg-gray-4 px-4 h-10 rounded-2xl placeholder:text-gray-500 text-white left-16" required autoComplete="off" disabled={loading || result?.success} value={(result?.success && result?.message) || emailRef.value}/>
                 <button className="ml-2 rounded-2xl bg-neutral-800 text-gray-600 outline-none flex items-center justify-center hover:bg-gray-4 p-2">
                     <svg className="w-6 h-6  " width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8l-8-8z"/></svg>
+                    {!loading && (
+                    (!result?.success) ||
+                    (result?.success )
+                    )}
+                    {loading }
                 </button>
+                {(!loading && result?.message && !result?.success) && (
+                   <div className="mt-2.5 ml-3 text-xs text-red-500">{result.message}</div>
+                )}
+              <ReCAPTCHA
+                 ref={recaptchaRef}
+                 size="invisible"
+                 theme="dark"
+                 sitekey="6LcfZyUeAAAAAN7bjLWN7woeVRHDLsVypV_InnfP"
+               />
               </form>
             </div>
           </div>
