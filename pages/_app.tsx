242ff21ada0a1d4d5f3a069df578080489fe92f7
Sagnik Sahoo <sahoosagnik1@gmail.com>
2022-10-07 12:09:18 +0530

    Revert "Store waitlisted emails using supabase database"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,6 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ToastProvider } from 'react-toast-notifications'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ToastProvider>
-      <Component {...pageProps} />;
-    </ToastProvider>
-  )
+  return <Component {...pageProps} />;
 }
